0c39d740f9fd0ade743e341e8a720cda0b2452a9
weiting <tina.hsu0816@gmail.com>
2025-08-15 22:33:56 +0800

    add stream handler

--- main.py
+++ main.py
@@ -1,16 +1,10 @@
-from time import time, sleep
-import sys, smbus
-import requests
-from mfrc522 import SimpleMFRC522
-import RPi.GPIO as GPIO
-from RPLCD.i2c import CharLCD
-import threading
-from datetime import datetime
 import firebase_admin
 from firebase_admin import credentials, db
-from evdev import InputDevice, categorize, ecodes
+from time import time, sleep
+import RPi.GPIO as GPIO
+import threading
+import multiprocessing
 
-sys.modules['smbus'] = smbus
 
 # 初始化 Firebase
 cred = credentials.Certificate("function-test-firebase-adminsdk.json")
@@ -18,194 +12,36 @@ firebase_admin.initialize_app(cred, {
     'databaseURL': 'https://function-test-4bdd0-default-rtdb.firebaseio.com/'
 })
 
-device_path = '/dev/input/event0'
-dev = InputDevice(device_path)
 
-GPIO.setmode(GPIO.BCM)
-LED_green = 21
-LED_red = 20
-button = 26
-Buzzer = 19
-IR_PIN = 16
-num = ""
-temporary_num = ""
+def stream_handler(event):
+    print("資料變更：")
+    print("Path:", event.path)
+    print("Data:", event.data)
 
-# 對應的數字鍵盤 keycode
-keypad_mapping = {
-    69: 'Num Lock',
-    98: '/',
-    55: '*',
-    74: '-',
-    78: '+',
-    14: 'Backspace',
-    96: 'Enter',
-    82: '0',
-    83: '.',
-    79: '1',
-    80: '2',
-    81: '3',
-    75: '4',
-    76: '5',
-    77: '6',
-    71: '7',
-    72: '8',
-    73: '9'
-}
+# 監聽 status 欄位
+def stream_handler_listener():
+    status_ref = db.reference('locks/lock_001/status')
+    status_ref.listen(stream_handler)
 
-
-GPIO.setup(LED_green, GPIO.OUT)
-GPIO.setup(LED_red, GPIO.OUT)
-GPIO.setup(button, GPIO.IN, GPIO.PUD_UP)
-GPIO.setup(Buzzer, GPIO.OUT)
-GPIO.setup(IR_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-
-lcd = CharLCD('PCF8574', address=0x27, port=1, backlight_enabled=True)
-lcd.clear()
-reader = SimpleMFRC522()
-
-CL = [0, 131, 147, 165, 175, 196, 220, 247]
-CM = [0, 262, 294, 330, 349, 392, 440, 494]
-CH = [0, 523, 587, 659, 698, 784, 880, 988]
-
-melody_doorBell = [CH[3], CH[1]]
-beat_doorBell = [1, 2]
-Buzz = GPIO.PWM(Buzzer, 440)
-
-###
-def get_access():
-    while True:
-        data = db.reference('locks/lock_001/status').get()
-    
-
-def get_access_settings():
-    data = db.reference('locks/lock_001/passwords').get()
-    return (
-        int(data.get("cardUID", 0)),
-        data.get("password", ""),
-        data.get("allow_start_time", ""),
-        data.get("allow_end_time", "")
-    )
-
-def update_lock_status(opened: bool):
-    db.reference('lock_status').set({'opened': opened})
-
-def log_access(status, method):
-    db.reference('access_log').push({
-        'status': status,
-        'method': method,
-        'timestamp': str(datetime.now())
-    })
-
-def myCallback(channel):
-    Buzz.start(50)
-    for i in range(len(melody_doorBell)):
-        Buzz.ChangeFrequency(melody_doorBell[i])
-        sleep(beat_doorBell[i] * 0.5)
-    Buzz.stop()
-
-GPIO.add_event_detect(button, GPIO.FALLING, callback=myCallback, bouncetime=350)
-
-def binary_aquire(pin, duration):
-    t0 = time()
-    results = []
-    while (time() - t0) < duration:
-        results.append(GPIO.input(pin))
-    return results
-
-def AllowedToEnter(method="unknown"):
-    GPIO.output(LED_green, GPIO.HIGH)
-    lcd.clear()
-    lcd.write_string("Welcome")
-    sleep(1)
-    GPIO.output(LED_green, GPIO.LOW)
-    lcd.clear()
-    update_lock_status(True)
-    log_access("allowed", method)
-
-def NotAllowedToEnter(method="unknown"):
-    GPIO.output(LED_red, GPIO.HIGH)
-    lcd.clear()
-    lcd.write_string("not allowed")
-    lcd.cursor_pos = (1, 7)
-    lcd.write_string("to enter")
-    sleep(1)
-    GPIO.output(LED_red, GPIO.LOW)
-    lcd.clear()
-    update_lock_status(False)
-    log_access("denied", method)
-
-def is_now_in_range(start_str, end_str):
-    now = datetime.now()
-    start = datetime.strptime(start_str, "%Y/%m/%d %H:%M")
-    end = datetime.strptime(end_str, "%Y/%m/%d %H:%M")
-    return start <= now <= end
-
-def keyboard_job():
-    for event in dev.read_loop():
-        if event.type == ecodes.EV_KEY:
-            key_event = categorize(event)
-            if key_event.keystate == key_event.key_down:
-                keycode = key_event.scancode
-                if keycode in keypad_mapping:
-                    print(f'You pressed: {keypad_mapping[keycode]}')
-
-def IR_job():
-    global temporary_num, num
-    while True:
-        GPIO.wait_for_edge(IR_PIN, GPIO.FALLING)
-        sleep(0.02)
-        code = on_ir_receive(IR_PIN)
-        if code is None:
-            continue
-        key = KEYS.get(code, None)
-        if key == 'EQ':
-            num = temporary_num
-            temporary_num = ""
-            cardUID, password, start_time, end_time = get_access_settings()
-            if num == password and is_now_in_range(start_time, end_time):
-                AllowedToEnter("password")
-            else:
-                NotAllowedToEnter("password")
-        elif key is not None:
-            temporary_num += key
-            if len(temporary_num) > 4:
-                temporary_num = temporary_num[1:]
-            lcd.clear()
-            lcd.write_string(temporary_num)
-
-def RFID_job():
-    while True:
-        id, text = reader.read()
-        cardUID, password, start_time, end_time = get_access_settings()
-        if id == cardUID and is_now_in_range(start_time, end_time):
-            AllowedToEnter("RFID")
-        else:
-            NotAllowedToEnter("RFID")
-        sleep(1)
-        lcd.clear()
-
-def destroy():
-    Buzz.stop()
-    GPIO.output(Buzzer, 1)
-    GPIO.cleanup()
-    lcd.clear()
 
 def main():
+    p = multiprocessing.Process(target=stream_handler_listener)
+    p.start()
+
     try:
-        lcd.clear()
-        lcd.write_string("Enter password")
-        lcd.cursor_pos = (1, 5)
-        lcd.write_string("or tap card")
-        get_access_settings()
-        threading.Thread(target=keyboard_job).start()
-        threading.Thread(target=RFID_job).start()
-        threading.Thread(target=get_access).start()
         while True:
+            db.reference('locks/lock_001/status').set(True)
             sleep(1)
+            db.reference('locks/lock_001/status').set(False)
+            sleep(1)
+            
     except KeyboardInterrupt:
-        print("Exit by Ctrl+C")
-    finally:
-        destroy()
+        print("停止監聽")
+        p.terminate()
+        p.join()
+        print("監聽已停止")
+    # finally:
+    #     GPIO.cleanup()
 
 if __name__ == "__main__":
     main()
