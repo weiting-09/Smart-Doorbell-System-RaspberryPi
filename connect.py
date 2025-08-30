c979d7dc07e0c01e8e7243adc57c503b4d0e2d6c
weiting <tina.hsu0816@gmail.com>
2025-08-30 11:39:35 +0800

    generate rasberry pi cpu id (connect.py)

--- connect.py
+++ connect.py
@@ -9,5 +9,21 @@ def generate_safe_id():
     chars = string.ascii_letters + string.digits + "@#$%^&*_-+="
     for _ in range(length):
         safe_id += secrets.choice(chars)
+    
+    get_raspberry_pi_id()
 
     return safe_id
+
+#取得 Raspberry Pi CPU 序號
+def get_raspberry_pi_id():
+    try:
+        with open("/proc/cpuinfo", "r") as f:
+            for line in f:
+                if line.startswith("Serial"):
+                    serial = line.strip().split(":")[1].strip()
+                    print("This Raspberry Pi's ID:", serial)
+                    return serial
+    except FileNotFoundError:
+        print("Cannot get Raspberry Pi ID")
+        return None
+    
