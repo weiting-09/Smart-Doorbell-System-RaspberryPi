16c735aa6fca922f40b0f98c8d16318b8f24dafb
weiting <tina.hsu0816@gmail.com>
2025-09-13 15:45:58 +0800

    fix conflict on RFID(register and verify) adjust LCD display time

--- RFID.py
+++ RFID.py
@@ -1,3 +1,4 @@
+import queue
 import time
 from access_control import allowed_to_enter, not_allowed_to_enter
 import constants
@@ -10,11 +11,23 @@ GPIO.setmode(GPIO.BCM)
 
 reader = SimpleMFRC522()
 
+# 建立全域 Queue
+rfid_queue = queue.Queue()
+
+# Producer: RFID Reader
+def rfid_reader_job():
+    while True:
+        id = reader.read_id()
+        if id:
+            print(f"[Reader] 感應到卡片 {id}")
+            rfid_queue.put(id)   # 丟進 Queue
+        time.sleep(0.5)  # 避免 CPU 過高
+
 def RFID_job():
     print("Starting RFID job...")
     while True:
         print("請將RFID卡片靠近讀取器...")
-        id = reader.read_id()
+        id = rfid_queue.get()
         if id is not None:
             print(f"RFID ID: {id}")
         time.sleep(0.5)
@@ -26,12 +39,13 @@ def RFID_job():
             print("RFID not recognized, not allowed to enter.")
             not_allowed_to_enter(method="RFID")
 
+#TODO: 增加計時器，未在一定時間內新增完成即關閉新增功能
 def add_new_RFID():
     ref = db.reference(f'locks/{constants.lock_id}/RFIDs/')
     while(True):
         try:
             print("請將新的RFID卡片靠近讀取器...")
-            id = reader.read_id()
+            id = rfid_queue.get()
             if id:
                 ref_card = ref.child(str(id))
                 if ref_card.get() is None:
